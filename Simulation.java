5015020155637f49021aafefbfb04656da2c6451
lev-l <balashov.leo@gmail.com>
2026-05-05 09:46:27 +0300

    Initial setup

--- Simulation.java
+++ Simulation.java
@@ -1,27 +1,37 @@
 public class Simulation {
     public static void main(String[] args) {
+        Visualization visual = new Visualization();
+
         Pendulum pendulum = new Pendulum(1.0, new Vector(0, 0),
                                         new Vector(0, 0), Math.PI / 4, 1.0);
         double timeStep = 0.01; // 10 milliseconds per update.
         System.out.println("Initial position: " + pendulum.getPosition());
         System.out.println("Initial inclination: " + pendulum.getInclination());
 
-        pendulum.update(timeStep);
-        System.out.println("Position after first update: " + pendulum.getPosition());
-        System.out.println("Inclination after first update: " + pendulum.getInclination());
+        //pendulum.update(timeStep);
+        //System.out.println("Position after first update: " + pendulum.getPosition());
+        //System.out.println("Inclination after first update: " + pendulum.getInclination());
 
         // Simulates quorter of the period for the initial conditions.
-        double elapsedTime = timeStep;
-        while(elapsedTime < 0.52){
-            pendulum.update(timeStep);
-            System.out.println(pendulum.getInclination() * 180 / Math.PI);
-            elapsedTime += timeStep;        
-        }
+        //double elapsedTime = timeStep;
+        //while(elapsedTime < 0.52){
+        //    pendulum.update(timeStep);
+        //    System.out.println(pendulum.getInclination() * 180 / Math.PI);
+        //    elapsedTime += timeStep;        
+        //}
 
-        System.out.println("Final position: " + pendulum.getPosition());
-        System.out.println("Final inclination: " + pendulum.getInclination()); // * 180 / Math.PI);
-        if(Math.round(pendulum.getPosition().getMagnitude()) != 1.0){
-            System.out.println("Error in the pendulum's length");
-        }
+        //System.out.println("Final position: " + pendulum.getPosition());
+        //System.out.println("Final inclination: " + pendulum.getInclination()); // * 180 / Math.PI);
+        //if(Math.round(pendulum.getPosition().getMagnitude()) != 1.0){
+        //    System.out.println("Error in the pendulum's length");
+        //}
+
+        //while(elapsedTime < 0.52 * 4){
+        //    pendulum.update(timeStep);
+        //    System.out.println(pendulum.getInclination() * 180 / Math.PI);
+        //    elapsedTime += timeStep;
+        //}
+
+        //System.out.println("Final position: " + pendulum.getPosition());
     }
 }
--- Visualization.java
+++ Visualization.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
+
 public class Visualization {
-    
+    private JFrame frame;
+
+    public Visualization()
+    {
+        frame = new JFrame("Pendulum Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
